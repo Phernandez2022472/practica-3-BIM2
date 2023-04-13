e285cb6effc34d21c47f1355312e636b0a6a884e
pablo David Hernandez ajiataz <128551949+Phernandez2022472@users.noreply.github.com>
2023-04-13 14:44:44 -0600

    Add files via upload

---
+++ barra_de_lads/barra_de_lads.ino
@@ -0,0 +1,121 @@
+#define LED1 2
+#define LED2 3
+#define LED3 4
+#define LED4 5
+#define LED5 6
+#define LED6 7
+#define LED7 8
+#define LED8 9
+#define LED9 10
+#define LED10 11
+void setup() 
+{
+ pinMode(LED1,OUTPUT);
+ pinMode(LED2,OUTPUT);
+ pinMode(LED3,OUTPUT);
+ pinMode(LED4,OUTPUT);
+ pinMode(LED5,OUTPUT);
+ pinMode(LED6,OUTPUT);
+ pinMode(LED7,OUTPUT);
+ pinMode(LED8,OUTPUT);
+ pinMode(LED9,OUTPUT);
+ pinMode(LED10
+ ,OUTPUT);
+ 
+
+}
+
+void loop()  
+{
+ digitalWrite(LED1,HIGH);//1
+ delay(50);
+ digitalWrite(LED1,LOW);
+ delay(10);
+ 
+ digitalWrite(LED2,HIGH);//2
+ delay(50);
+ digitalWrite(LED2,LOW);
+ delay(10);
+ 
+ digitalWrite(LED3,HIGH);//3
+ delay(50);
+ digitalWrite(LED3,LOW);
+ delay(10);
+ 
+ digitalWrite(LED4,HIGH);//4
+ delay(50);
+ digitalWrite(LED4,LOW);
+ delay(10);
+ 
+ digitalWrite(LED5,HIGH);//5
+ delay(50);
+ digitalWrite(LED5,LOW);
+ delay(10);
+ 
+ digitalWrite(LED6,HIGH);//6
+ delay(50);
+ digitalWrite(LED6,LOW);
+ delay(10);
+ 
+ digitalWrite(LED7,HIGH);//7
+ delay(50);
+ digitalWrite(LED7,LOW);
+ delay(10);
+ 
+ digitalWrite(LED8,HIGH);//8
+ delay(50);
+ digitalWrite(LED8,LOW);
+ delay(10);
+ 
+ digitalWrite(LED9,HIGH);//9
+ delay(50);
+ digitalWrite(LED9,LOW);
+ delay(10);
+ 
+ digitalWrite(LED10,HIGH);//10
+ delay(50);
+ digitalWrite(LED10,LOW);
+ delay(10);
+
+ digitalWrite(LED9,HIGH);//9
+ delay(50);
+ digitalWrite(LED9,LOW);
+ delay(10);
+ 
+ digitalWrite(LED8,HIGH);//8
+ delay(50);
+ digitalWrite(LED8,LOW);
+ delay(10);
+ 
+ digitalWrite(LED7,HIGH);//7
+ delay(50);
+ digitalWrite(LED7,LOW);
+ delay(10);
+ 
+ digitalWrite(LED6,HIGH);//6
+ delay(50);
+ digitalWrite(LED6,LOW);
+ delay(10);
+ digitalWrite(LED5,HIGH);//5
+ delay(50);
+ digitalWrite(LED5,LOW);
+ delay(10);
+ digitalWrite(LED4,HIGH);//4
+ delay(50);
+ digitalWrite(LED4,LOW);
+ delay(10);
+  digitalWrite(LED3,HIGH);//3
+ delay(50);
+ digitalWrite(LED3,LOW);
+ delay(10);
+ 
+ digitalWrite(LED2,HIGH);//2
+ delay(50);
+ digitalWrite(LED2,LOW);
+ delay(10);
+ digitalWrite(LED1,HIGH);//1
+ delay(50);
+ digitalWrite(LED1,LOW);
+ delay(10);
+ 
+}
